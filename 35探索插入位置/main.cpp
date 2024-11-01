3ec5a001de1b284ca4ad6ad93c475ad24a3fe3df
KB初岚 <145010602+KBchulan@users.noreply.github.com>
2024-11-01 16:01:20 +0800

    11-1

---
+++ 35探索插入位置/main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    static int searchInsert(vector<int> &nums, int target)
+    {
+        for(auto i = 0; i < nums.size(); i++){
+            if(nums[i] >= target)
+                return i;
+        }
+        return nums.size();
+    }
+};
+
+int main()
+{
+    vector<int> nums = {1, 3, 5, 6};
+    int target = 5;
+    cout << Solution::searchInsert(nums, target) << endl;
+}
